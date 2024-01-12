a9e23d9bcd2e66274808460fb975c7f05fe8aa2d
TagYurIt <151072443+TagYurIt@users.noreply.github.com>
2024-01-12 13:36:44 -0700

    Update rpg_game_colourless.sh to 2.0

--- rpg_game_colourless.sh
+++ rpg_game_colourless.sh
@@ -41,7 +41,103 @@ do
     beasthp=$(( ( $RANDOM % 16 ) + 10 ))
 
     # beast script
-    echo "A beast has appeared!"
+    scene=$(( $RANDOM % 30 ))
+
+    case $scene in
+        0)
+            echo "You walk into a forest and find a beast!"
+        ;;
+        1)
+            echo "You go into the long grass and find a beast!"
+        ;;
+        2)
+            echo "You find a castle and at the door you are greeted by a beast!"
+        ;;
+        3)
+            echo "You are walking through a castle and you find a beast!"
+        ;;
+        4)
+            echo "A wild beast appeared!"
+        ;;
+        5)
+            echo "You wander into a cave and a beast appears from around the corner!"
+        ;;
+        6)
+            echo "While wandering around a mansion you encounter a beast!"
+        ;;
+        7)
+            echo "While looking around in a forest a beast starts running towards you!"
+        ;;
+        8)
+            echo "You find a mansion, and you are greeted by a beast at the door!"
+        ;;
+        9)
+            echo "While spelunking you are met with a beast!"
+        ;;
+        10)
+            echo "While leaving a forest a beast runs up from behind you!"
+        ;;
+        11)
+            echo "While checking out a cave you are met with a beast!"
+        ;;
+        12)
+            echo "While scouting out an area, you see a beast running towards you!"
+        ;;
+        13)
+            echo "You are looking around a dungeon and a beast starts to approach you!"
+        ;;
+        14)
+            echo "You were looking around an abandoned town, and you spotted a beast coming right towards you!"
+        ;;
+        15)
+            echo "You were exploring an abandoned building and you found a beast!"
+        ;;
+        16)
+            echo "You were exploring a cave with masks on the wall, and you spotted a beast!"
+        ;;
+        17)
+            echo "You killed a beast and out of its corpse comes another beast!"
+        ;;
+        18)
+            echo "While exploring the Chernobyl exclusion zone you encounter a beast!"
+        ;;
+        19)
+            echo "You look into the door of an abandoned building, and you are met by a beast!"
+        ;;
+        20)
+            echo "You're walking around a cemetery and you encounter a beast!"
+        ;;
+        21)
+            echo "You are wandering around the courtyard of a mansion, and you encounter a beast!"
+        ;;
+        22)
+            echo "You stumbled upon an abandoned building and a beast appeared!"
+        ;;
+        23)
+            echo "You investigate a cupboard in an abandoned building, and you find a beast!"
+        ;;
+        24)
+            echo "You were riding around on your mobility scooter, and you found a beast!"
+        ;;
+        25)
+            echo "You found a bunch of peach trees, but then you saw a beast running towards you!"
+        ;;
+        26)
+            echo "You fell into a pit and there was a beast!"
+        ;;
+        27)
+            echo "You were questioning your life choices when a beast spawned in!"
+        ;;
+        28)
+            echo "While eating microplastics a beast appeared!"
+        ;;
+        29)
+            echo "While out at night drunk as hell you ran into a beast!"
+        ;;
+        30)
+            echo "I don't know. Here's a beast!"
+        ;;
+    esac
 
     while [[ $beasthp -ge 1 ]]
     do
